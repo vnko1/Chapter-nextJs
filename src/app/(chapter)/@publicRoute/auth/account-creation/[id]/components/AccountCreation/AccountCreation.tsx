c14b5f56910412d9d66fb835c225b3f9767668ab
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-17 14:38:46 +0300

    save

--- src/app/(chapter)/@publicRoute/auth/account-creation/[id]/components/AccountCreation/AccountCreation.tsx
+++ src/app/(chapter)/@publicRoute/auth/account-creation/[id]/components/AccountCreation/AccountCreation.tsx
@@ -1,7 +1,14 @@
-import React, { FC } from "react";
+"use client";
+import { ChangeEvent, FC, useState, useEffect } from "react";
+import { AxiosError } from "axios";
+import { Formik, Form, FormikProps, FormikHelpers } from "formik";
+import cn from "classnames";
+
 import { AccountCreationProps } from "./AccountCreation.type";
+import { useRouter } from "next/navigation";
 
 const AccountCreation: FC<AccountCreationProps> = ({ id }) => {
+  const router = useRouter();
   return <div>AccountCreation:{id}</div>;
 };
 
