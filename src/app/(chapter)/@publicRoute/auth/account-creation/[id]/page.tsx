484195d8dde938989029fd35f9d7d69d99f74a7f
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-17 14:03:26 +0300

    save

---
+++ src/app/(chapter)/@publicRoute/auth/account-creation/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AccountCreationId() {
+  return <div>AccountCreationIdPage</div>;
+}
+
+export default AccountCreationId;
--- src/app/(chapter)/@publicRoute/auth/account-creation/page.tsx
+++
@@ -1,5 +0,0 @@
-function Page() {
-  return <div>Acc Creation Page</div>;
-}
-
-export default Page;
