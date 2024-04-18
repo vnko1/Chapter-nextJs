3b60eee4fcf0aa85c604669fd2870a7fc6099de0
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-18 11:59:10 +0300

    save

--- src/context/Profile/ProfileProvider.tsx
+++ src/context/Profile/ProfileProvider.tsx
@@ -1,70 +1,20 @@
 "use client";
-import { FC, createRef, useEffect, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { AxiosResponse } from "axios";
 
-import {
-  INotification,
-  INots,
-  SocketEventsEnum,
-  PostRefType,
-  EndpointsEnum,
-  IUser,
-} from "@/types";
-import { SocketApi, clientApi } from "@/services";
+import { EndpointsEnum, IUser } from "@/types";
+import { clientApi } from "@/services";
 
 import { ProfileContext } from "./hooks";
 import { IProfileProviderProps } from "./ProfileProvider.type";
 import { getParsedSession } from "@/lib";
 
-const socket = new SocketApi();
-
 const ProfileProvider: FC<IProfileProviderProps> = ({ children }) => {
   const [isAuth, setIsAuth] = useState(false);
   const [token, setToken] = useState<string | null>(null);
   const [user, setUser] = useState<IUser | null>(null);
-  console.log(user);
-  const [isConnected, setIsConnected] = useState(false);
-  const [isLoading] = useState(false);
-  const [userPostsList, setUserPostsList] = useState<Array<PostRefType>>([]);
-  const [isLoad, setIsLoad] = useState(false);
-
   const [headerAddPostBtnIsDisabled, setHeaderAddPostBtnIsDisabled] =
     useState(false);
-
-  const [notifications, setNotifications] = useState<Array<INots>>([]);
-
-  const editedNotifications: Array<INotification> = notifications
-    .sort(
-      (a, b) =>
-        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-    )
-    .map((el) => ({
-      ...el,
-      nodeRef: createRef(),
-    }));
-
-  const viewedNotifications = editedNotifications.filter(
-    (notification) => notification.isViewed
-  );
-  const newNotifications = editedNotifications.filter(
-    (notification) => !notification.isViewed
-  );
-  const [unreadMessage, setUnreadMessage] = useState(newNotifications.length);
-
-  const [page, setPage] = useState<number>(0);
-
-  const fetchUserPosts = async (currentPage: number) => {
-    try {
-      const response = await clientApi.get(
-        `${EndpointsEnum.POSTS_BY_AUTHOR}?page=${currentPage}&limit=50`
-      );
-      setUserPostsList(response.data);
-      setIsLoad(true);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching user posts:", error);
-    }
-  };
 
   useEffect(() => {
     async function getMe() {
@@ -84,85 +34,14 @@ const ProfileProvider: FC<IProfileProviderProps> = ({ children }) => {
     });
   }, []);
 
-  // useLayoutEffect(() => {
-  //   setIsLoading(true);
-  //   clientApi
-  //     .get(EndpointsEnum.NOTA)
-  //     .then(({ data }: AxiosResponse<Array<INots>>) => setNotifications(data))
-  //     .catch((e) => {
-  //       if (e instanceof AxiosError) {
-  //         throw new Error(e.message);
-  //       }
-  //     })
-  //     .finally(() => setIsLoading(false));
-  // }, []);
-
-  useEffect(() => {
-    const onConnect = () => setIsConnected(true);
-    const onError = (error: Error) => console.log(error);
-    const onDisconnect = () => setIsConnected(false);
-
-    // if (token) {
-    //   socket.init(token);
-    //   socket.connect(isAuth);
-    // }
-
-    socket.addListener("connect", onConnect);
-    socket.addListener("connect_error", onError);
-    socket.addListener("disconnect", onDisconnect);
-
-    return () => {
-      socket.removeListener("connect", onConnect);
-      socket.addListener("connect_error", onError);
-      socket.removeListener("disconnect", onDisconnect);
-      socket.disconnect();
-    };
-  }, [isAuth, token]);
-
-  useEffect(() => {
-    const onHandleSubscribe = socket.handleData<INots>(setNotifications);
-
-    const onHandleNewPost = socket.handleData<INots>(setNotifications);
-
-    if (isConnected) {
-      socket.addListener<INots>(SocketEventsEnum.subscribe, onHandleSubscribe);
-
-      socket.addListener<INots>(SocketEventsEnum.post, onHandleNewPost);
-    }
-
-    return () => {
-      socket.removeListener<INots>(
-        SocketEventsEnum.subscribe,
-        onHandleSubscribe
-      );
-
-      socket.removeListener<INots>(SocketEventsEnum.post, onHandleNewPost);
-    };
-  }, [isConnected]);
-
-  useEffect(() => {
-    setUnreadMessage(newNotifications.length);
-  }, [newNotifications.length]);
-
   return (
     <ProfileContext.Provider
       value={{
         headerAddPostBtnIsDisabled,
-        unreadMessage,
-        viewedNotifications,
-        newNotifications,
-        isLoading,
-        notificationsLength: notifications.length,
         setHeaderAddPostBtnIsDisabled,
-        setUnreadMessage,
-        setNotifications,
-
-        page,
-        setPage,
-        fetchUserPosts,
-        userPostsList,
-        setUserPostsList,
-        isLoad,
+        isAuth,
+        token,
+        user,
       }}
     >
       {children}
--- src/context/Profile/ProfileProvider.type.ts
+++ src/context/Profile/ProfileProvider.type.ts
@@ -1,6 +1,5 @@
 import { Dispatch, ReactNode, SetStateAction } from "react";
-
-import { PostRefType, INotification, INots } from "@/types";
+import { IUser } from "@/types";
 
 export interface IProfileProviderProps {
   children: ReactNode;
@@ -10,18 +9,8 @@ type SetBoolean = Dispatch<SetStateAction<boolean>>;
 
 export type ProfileContextType = {
   headerAddPostBtnIsDisabled: boolean;
-  unreadMessage: number;
-  viewedNotifications: Array<INotification>;
-  newNotifications: Array<INotification>;
-  isLoading: boolean;
-  notificationsLength: number;
-  setNotifications: Dispatch<SetStateAction<Array<INots>>>;
   setHeaderAddPostBtnIsDisabled: SetBoolean;
-  setUnreadMessage: Dispatch<SetStateAction<number>>;
-  page: number;
-  setPage: Dispatch<SetStateAction<number>>;
-  fetchUserPosts: (currentPage: number) => void;
-  userPostsList: [] | Array<PostRefType>;
-  setUserPostsList: Dispatch<SetStateAction<Array<PostRefType>>>;
-  isLoad: boolean;
+  token: string | null;
+  isAuth: boolean;
+  user: IUser | null;
 };
