70094b01075df2ca466c2363b816bec1b97c5923
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-18 12:32:21 +0300

    save

--- src/services/clientApi/index.ts
+++ src/services/clientApi/index.ts
@@ -44,7 +44,7 @@ api.interceptors.response.use(
     ) {
       error.config._isRetry = true;
       try {
-        let apiBaseUrl: string = import.meta.env.VITE_API_BASE_URL;
+        let apiBaseUrl: string = BASE_URL;
         if (apiBaseUrl[apiBaseUrl.length - 1] !== "/") {
           apiBaseUrl += "/";
         }
