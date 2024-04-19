dc634631e267459b8382f8de617abbe5c3b7d7c2
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-04-19 20:01:52 +0300

    save

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -11,11 +11,8 @@ function Error() {
       <section className={styles["error"]}>
         <div className={`${styles["container"]} ${styles["error"]}`}>
           <h1 className={`${styles["error_title"]} ${styles["title"]}`}>
-            Oh no! Page not found
+            Oh no! Something went wrong!
           </h1>
-          <p className={`${styles["error__text"]} ${styles["text"]}`}>
-            Sorry, we couldn`t find the page you are looking for.
-          </p>
           <div className={styles["error__button"]}>
             <Link className="button" href="/">
               Go to home page
